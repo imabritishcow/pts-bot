3faa1f2af347ca5f4e0c0fca2d635a9369c1f5db
imabritishcow <m@bcow.tk>
2020-07-10 14:32:24 -0400

    Updated help for /pts

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
       .addField("/help", "Shows this")
       .addField("/faqs", "Shows frequently asked questions")
       .addField("/newac", "Creates a new card linked to your discord account")
-      .addField("/pts [user]", "Shows your card info")
+      .addField("/pts [user]", "Shows someones points. User is optional.")
       .setColor(0x2e3192)
       .setThumbnail('https://cdn.bcow.tk/logos/pts.png')
       .setFooter('pts', 'https://cdn.bcow.tk/logos/pts.png');
